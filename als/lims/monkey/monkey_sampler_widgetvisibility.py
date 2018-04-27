20adf7e2f130780ea61d632e10072a0eff9ba871
Lunga Baliwe <lunga001@gmail.com>
2018-04-27 02:10:40 +0200

    Inline edits for DateSampled and Sampler fields

--- als/lims/monkey/monkey_sampler_widgetvisibility.py
+++ als/lims/monkey/monkey_sampler_widgetvisibility.py
@@ -3,7 +3,7 @@ def __call__(self, context, mode, field, default):
     https://jira.bikalabs.com/browse/AN-182 so that Sampler can be 
     shown on AR Create
     """
-    fields = ['DateSampled', 'SamplingDate']
+    fields = ['SamplingDate']
     state = default if default else 'invisible'
     fieldName = field.getName()
     if fieldName not in fields:
@@ -20,11 +20,7 @@ def __call__(self, context, mode, field, default):
     # - DateSampled: invisible during creation.
     # - SamplingDate and Sampler: visible and editable until sample due.
     if swf_enabled:
-        if fieldName == 'DateSampled':
-            if mode == 'add':
-                state = 'invisible'
-                field.required = 0
-        elif fieldName in fields:
+        if fieldName in fields:
             if mode == 'header_table':
                 state = 'prominent'
             elif mode == 'view':
@@ -35,20 +31,6 @@ def __call__(self, context, mode, field, default):
     #                 required in 'add' view.
     #  - 'SamplingDate' and 'Sampler': disabled everywhere.
     else:
-        if fieldName == 'DateSampled':
-            if mode == 'add':
-                state = 'edit'
-                field.required = 1
-            elif mode == 'edit':
-                state = 'invisible'
-            elif mode == 'view':
-                state = 'visible'
-            elif mode == 'header_table':
-                # In the Schema definition, DateSampled is 'prominent' for
-                # 'header_table' to let users edit it after receiving
-                # the Sample. But if SWF is disabled, DateSampled must be
-                # filled during creation and never editable.
-                state = 'visible'
-        elif fieldName in fields:
+        if fieldName in fields:
             state = 'invisible'
     return state
